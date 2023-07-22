816c85ca46ba176b3c80db3dd85b33129f8c45d4
Alaa Elghamry <alaa.elghamry7@gmail.com>
2023-07-22 16:50:35 +0300

    Add Options Component

--- src/Question.js
+++ src/Question.js
@@ -1,18 +1,11 @@
+import Options from "./Options";
+
 function Question({question}) {
     return(
         <>
       <h4>{question.question}</h4>   
 
-      <div className="options">
-          <h1>fdfd</h1>
-        {question.options.map((option)=>{
-            return(
-<button className="btn btn-option" key={option}>
-             {option}
-</button>
-)
-        })}
-      </div>
+     <Options  question={question}/>
         </>
     );
 }
